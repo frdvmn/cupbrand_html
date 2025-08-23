f7f77cacb3ffece39b6c198966b427a1e2314690
Kalashnikov <frdvmn@mail.ru>
2025-08-23 21:07:23 +0300

    feat: завершил верстку subhero секции

--- src/js/components/swiper.js
+++ src/js/components/swiper.js
@@ -1,23 +1,33 @@
 import Swiper from 'swiper';
-import { Navigation, Pagination } from 'swiper/modules';
-Swiper.use([Navigation, Pagination]);
+import { Autoplay, Navigation, Pagination, Scrollbar} from 'swiper/modules';
+Swiper.use([Navigation, Pagination, Scrollbar, Autoplay]);
 const swiper = new Swiper('.hero__swiper', {
-  // Optional parameters
+  slidesPerView: 1,
   direction: 'horizontal',
   loop: true,
-
-
- // If we need pagination
   pagination: {
     el: '.swiper-pagination',
   },
-
-  // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
+});
 
-
+const subhero_swiper = new Swiper('.subhero__swiper', {
+  direction: 'horizontal',
+  loop: false,
+  slidesPerView: 2,
+  spaceBetween: 20,
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    // hide: true,
+    snapOnRelease: true,
+    draggable: true
+  },
+  autoplay: {
+    delay: 3000,
+    pauseOnMouseEnter: true
+  },
 
 });
