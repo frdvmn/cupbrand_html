e04aa9117b238438920ebb17d503451ef89fcc70
Kalashnikov <frdvmn@mail.ru>
2025-10-18 10:04:11 +0300

    feat(phone): add real-time Russian phone number formatting with libphonenumber-js

---
+++ src/js/components/phonenumber.js
@@ -0,0 +1,21 @@
+import { AsYouType } from 'libphonenumber-js';
+
+const input = document.getElementById('tel');
+input.addEventListener('input', (e) => {
+let digits = e.target.value.replace(/\D/g, '');
+
+if (digits.startsWith('8')) {
+  digits = '7' + digits.slice(1);
+} else if (digits && digits[0] === '9') {
+  digits = '7' + digits;
+}
+
+if (digits.length > 11) digits = digits.slice(0, 11);
+
+let formatted = '';
+if (digits) {
+  formatted = new AsYouType().input('+' + digits);
+}
+
+e.target.value = formatted;
+});
--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 import './components/swiper.js';
 import './components/fancybox.js';
 import './components/aos.js';
+import './components/phonenumber.js';
 import './functions/burger.js';
